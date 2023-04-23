dea371e426489715d975ddefa7ad273b128c7bb7
wenlong_12 <wenlong12@huawei.com>
2023-04-23 16:59:12 +0800

    re2版本升级到20211101      Signed-off-by: wenlong_12 <wenlong12@huawei.com>
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation version 2.1
+# of the License.
+#
+# Copyright(c) 2023 Huawei Device Co., Ltd.
+
+set -e
+cd $1
+if [ -d "re2-2021-11-01" ];then
+    rm -rf re2-2021-11-01
+fi
+tar zxvf re2-20211101.tar.gz
+exit 0
